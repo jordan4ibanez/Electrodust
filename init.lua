a753c042be76e85b3b4cc69df4db23a55a302104
jordan4ibanez <jordan4ibanez@gmail.com>
2017-10-03 15:38:39 +0000

    Dust textures

--- init.lua
+++ init.lua
@@ -12,11 +12,23 @@ inventory_image = "electrodust_dust.png",
 
 minetest.register_node("electrodust:dust_off", {
 	description = "Dust Off",
-	tiles = {"carts_rail_straight.png","carts_rail_curved.png","carts_rail_crossing.png","carts_rail_t_junction.png"},
+	tiles = {"electrodust_dust_off_straight.png","electrodust_dust_off_curved.png","electrodust_dust_off_tjunction.png","electrodust_dust_off_crossing.png"},
 	drawtype = "raillike",
 	is_ground_content = true,
 	groups = {cracky=3, stone=1},
-	--drop = 'default:cobble',
+	drop = "electrodust:dust",
+	legacy_mineral = true,
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("electrodust:dust_on", {
+	description = "Dust On",
+	tiles = {"electrodust_dust_on_straight.png","electrodust_dust_on_curved.png","electrodust_dust_on_tjunction.png","electrodust_dust_on_crossing.png"},
+	drawtype = "raillike",
+	light_source = 14,
+	is_ground_content = true,
+	groups = {cracky=3, stone=1},
+	drop = "electrodust:dust",
 	legacy_mineral = true,
 	sounds = default.node_sound_stone_defaults(),
 })
